81847d0dc2e31a7efc5bdbeaa6620d9e7284f6cc
Юлия Фомина <100119458+FominaYuliya@users.noreply.github.com>
2026-04-20 23:39:14 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -93,73 +93,8 @@ scrollTopBtn.addEventListener("click", function() {
     window.scrollTo({ top: 0, left: 0, behavior: "smooth"})});
 
 
-// КНОПКА БУРГЕР
-document.addEventListener('DOMContentLoaded', function (){
-    const burger = document.getElementById('burger');
-    const navMenu = document.getElementById('navMenu');
 
-    burger.addEventListener('click', function () {
-        burger.classList.toggle('active');
-        navMenu.classList.toggle('active');
 
-        document.body.style.overflow = navMenu.classList.contains
-        ('active') ? 'hidden' : '';
-    });
 
-    const navLinks = document.querySelectorAll('.nav_menu a');
-    navLinks.forEach(link => {
-        link.addEventListener('click', function (){
-            burger.classList.remove('active');
-            navMenu.classList.remove('active');
-            document.body.style.overflow = '';
-        });
-    });
-    document.addEventListener('click', function (e) {
-        if(!navMenu.contains(e.target) && !burger.contains(e.target)
-            && navMenu.classList.contains('active')) {
-            burger.classList.remove('active');
-            navMenu.classList.remove('active');
-            document.body.style.overflow = '';
-        }
-    });
-});
-
-//МОДАЛЬНОЕ ОКНО
-// Получаем необходимые элементы
-const openModalButton = document.getElementById('openModal');
-const modalOverlay = document.getElementById('modalOverlay');
-const closeModalButton = document.getElementById('closeModal');
-const cancelModalButton = document.getElementById('cancelModal');
-
-// Функция открытия модального окна
-function openModal() {
-    modalOverlay.classList.add('active');
-    document.body.style.overflow = 'hidden'; // Запрещаем прокрутку страницы
-}
-
-// Функция закрытия модального окна
-function closeModal() {
-    modalOverlay.classList.remove('active');
-    document.body.style.overflow = ''; // Разрешаем прокрутку страницы
-}
-
-// Обработчики событий для кнопок
-openModalButton.addEventListener('click', openModal);
-closeModalButton.addEventListener('click', closeModal);
-cancelModalButton.addEventListener('click', closeModal);
-
-// Закрытие модального окна при клике на оверлей
-modalOverlay.addEventListener('click', function(event) {
-    if (event.target === modalOverlay) {
-        closeModal();
-    }
-});
-
-// Закрытие модального окна при нажатии Escape
-document.addEventListener('keydown', function(event) {
-    if (event.key === 'Escape' && modalOverlay.classList.contains('active')) {
-        closeModal();
-    }
-});
 
 
